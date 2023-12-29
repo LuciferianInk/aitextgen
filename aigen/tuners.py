93c6fdb327dd8ca3205e395a2929d50e41024078
Luciferian Ink <LuciferianInk@protonmail.com>
2023-12-29 07:12:48 -0600

    syntax

--- aigen/tuners.py
+++ aigen/tuners.py
@@ -18,135 +18,124 @@ from .aigen import aigen
 from .utils import colors
 
 
-class Objective:
-    def __init__(self, init_kwargs, train_config):
-        self.init_kwargs = init_kwargs
-        self.train_config = train_config
+def objective(trial: optuna.trial.Trial, init_kwargs, train_config):
+    train_config["num_steps"] = 25
 
-        self.train_config["num_steps"] = 25
+    max_batch_size = (
+        train_config.get("batch_size", 1)
+        * train_config.get("gradient_accumulation_steps", 1)
+        * train_config.get("target_batch_size", 1)
+    )
+    min_batch_size = 128
 
-        self.max_batch_size = (
-            self.train_config.get("batch_size", 1)
-            * self.train_config.get("gradient_accumulation_steps", 1)
-            * self.train_config.get("target_batch_size", 1)
+    train_config["batch_size"] = 1
+
+    train_type = train_config.get("type", "standard")
+
+    if train_type in ["pretrain"]:
+        setattr(init_kwargs["config"], "n_head", trial.suggest_int("n_head", 1, 2))
+        setattr(init_kwargs["config"], "k_att", trial.suggest_int("k_att", 2, 3))
+        setattr(init_kwargs["config"], "k_mlp", trial.suggest_int("k_mlp", 2, 3))
+        setattr(
+            init_kwargs["config"],
+            "sample_topk",
+            trial.suggest_int("sample_topk", 1, 2),
         )
-        self.min_batch_size = 128
-
-        self.train_config["batch_size"] = 1
-
-    def __call__(self, trial: optuna.trial.Trial):
-        train_type = self.train_config.get("type", "standard")
-
-        if train_type in ["pretrain"]:
-            setattr(
-                self.init_kwargs["config"], "n_head", trial.suggest_int("n_head", 1, 2)
-            )
-            setattr(
-                self.init_kwargs["config"], "k_att", trial.suggest_int("k_att", 2, 3)
-            )
-            setattr(
-                self.init_kwargs["config"], "k_mlp", trial.suggest_int("k_mlp", 2, 3)
-            )
-            setattr(
-                self.init_kwargs["config"],
-                "sample_topk",
-                trial.suggest_int("sample_topk", 1, 2),
-            )
-            setattr(
-                self.init_kwargs["config"],
-                "gate_type",
-                trial.suggest_categorical("gate_type", ["mlp", "gmm"]),
-            )
-            block_size = trial.suggest_int("block_size", 64, 256, step=64)
-            setattr(
-                self.init_kwargs["config"],
-                "block_size",
-                block_size,
-            )
-            setattr(
-                self.init_kwargs["config"],
-                "history_length",
-                block_size,
-            )
-            setattr(
-                self.init_kwargs["config"],
-                "gating_size",
-                int(block_size / 2),
-            )
-            setattr(
-                self.init_kwargs["config"],
-                "aux_loss_weight",
-                trial.suggest_float("aux_loss_weight", 0.00001, 0.1),
-            )
-            setattr(
-                self.init_kwargs["config"],
-                "resid_pdrop",
-                trial.suggest_float("resid_pdrop", 0, 0.9),
-            )
-            setattr(
-                self.init_kwargs["config"],
-                "embd_pdrop",
-                trial.suggest_float("embd_pdrop", 0, 0.9),
-            )
-            setattr(
-                self.init_kwargs["config"],
+        setattr(
+            init_kwargs["config"],
+            "gate_type",
+            trial.suggest_categorical("gate_type", ["mlp", "gmm"]),
+        )
+        block_size = trial.suggest_int("block_size", 64, 256, step=64)
+        setattr(
+            init_kwargs["config"],
+            "block_size",
+            block_size,
+        )
+        setattr(
+            init_kwargs["config"],
+            "history_length",
+            block_size,
+        )
+        setattr(
+            init_kwargs["config"],
+            "gating_size",
+            int(block_size / 2),
+        )
+        setattr(
+            init_kwargs["config"],
+            "aux_loss_weight",
+            trial.suggest_float("aux_loss_weight", 0.00001, 0.1),
+        )
+        setattr(
+            init_kwargs["config"],
+            "resid_pdrop",
+            trial.suggest_float("resid_pdrop", 0, 0.9),
+        )
+        setattr(
+            init_kwargs["config"],
+            "embd_pdrop",
+            trial.suggest_float("embd_pdrop", 0, 0.9),
+        )
+        setattr(
+            init_kwargs["config"],
+            "attn_pdrop",
+            trial.suggest_float(
                 "attn_pdrop",
-                trial.suggest_float(
-                    "attn_pdrop",
-                    0,
-                    0.9,
-                ),
-            )
-            setattr(
-                self.init_kwargs["config"],
-                "moe_pdrop",
-                trial.suggest_float("moe_pdrop", 0, 0.9),
-            )
-
-        self.train_config["optimizer"] = trial.suggest_categorical(
-            "optimizer", ["AdamW", "Lion"]
+                0,
+                0.9,
+            ),
         )
-        self.train_config["warmup_steps"] = trial.suggest_int("warmup_steps", 0, 10)
-        self.train_config["learning_rate"] = trial.suggest_float(
-            "learning_rate", 0.00001, 0.1, log=True
-        )
-        self.train_config["gradient_accumulation_steps"] = trial.suggest_int(
-            "gradient_accumulation_steps",
-            self.min_batch_size,
-            self.max_batch_size,
-            step=self.min_batch_size,
-        )
-        self.train_config["weight_decay"] = trial.suggest_float(
-            "weight_decay", 0.00001, 0.1, log=True
+        setattr(
+            init_kwargs["config"],
+            "moe_pdrop",
+            trial.suggest_float("moe_pdrop", 0, 0.9),
         )
 
-        if train_type in ["lora"]:
-            self.train_config["r"] = trial.suggest_int("r", 1, 64)
-            self.train_config["alpha"] = trial.suggest_int("alpha", 1, 32)
-            self.train_config["dropout"] = trial.suggest_float("dropout", 0, 0.5)
-            self.train_config["bias"] = trial.suggest_categorical(
-                "bias", ["none", "all", "lora_only"]
-            )
+    train_config["optimizer"] = trial.suggest_categorical(
+        "optimizer", ["AdamW", "Lion"]
+    )
+    train_config["warmup_steps"] = trial.suggest_int("warmup_steps", 0, 10)
+    train_config["learning_rate"] = trial.suggest_float(
+        "learning_rate", 0.00001, 0.1, log=True
+    )
+    train_config["gradient_accumulation_steps"] = trial.suggest_int(
+        "gradient_accumulation_steps",
+        min_batch_size,
+        max_batch_size,
+        step=min_batch_size,
+    )
+    train_config["weight_decay"] = trial.suggest_float(
+        "weight_decay", 0.00001, 0.1, log=True
+    )
 
-        self.prototype = aigen(**self.init_kwargs)
-
-        if train_type not in ["standard", "pretrain"]:
-            self.prototype.create_adapter(self.train_config)
-
-        os.makedirs(f"{self.train_config['log_path']}/tune", exist_ok=True)
-        logger = loggers.TensorBoardLogger(
-            self.train_config["log_path"], name="tune", default_hp_metric=True
+    if train_type in ["lora"]:
+        train_config["r"] = trial.suggest_int("r", 1, 64)
+        train_config["alpha"] = trial.suggest_int("alpha", 1, 32)
+        train_config["dropout"] = trial.suggest_float("dropout", 0, 0.5)
+        train_config["bias"] = trial.suggest_categorical(
+            "bias", ["none", "all", "lora_only"]
         )
 
-        train_loss = self.prototype.train(
-            loggers=[logger],
-            callbacks=[CustomPruningCallback(trial, monitor="train_loss")],
-            trial=True,
-            verbose=False,
-            **self.train_config,
-        )
+    prototype = aigen(**init_kwargs)
 
-        return train_loss
+    if train_type not in ["standard", "pretrain"]:
+        prototype.create_adapter(train_config)
+
+    os.makedirs(f"{train_config['log_path']}/tune", exist_ok=True)
+    logger = loggers.TensorBoardLogger(
+        train_config["log_path"], name="tune", default_hp_metric=True
+    )
+
+    train_loss = prototype.train(
+        loggers=[logger],
+        callbacks=[CustomPruningCallback(trial, monitor="train_loss")],
+        trial=True,
+        verbose=False,
+        **train_config,
+    )
+
+    return train_loss
 
 
 _EPOCH_KEY = "ddp_pl:epoch"
@@ -219,11 +208,13 @@ class CustomPruningCallback(PyTorchLightningPruningCallback):
 
 def optimize_hparams(init_kwargs, train_config):
     storage = optuna.storages.RDBStorage(
+        # url="sqlite:///trial.db",
         url="sqlite:///:memory:",
         engine_kwargs={"pool_size": 20, "connect_args": {"timeout": 10}},
     )
 
     study = optuna.create_study(
+        study_name="trial-1",
         direction="minimize",
         storage=storage,
         sampler=optuna.samplers.TPESampler(),
@@ -232,7 +223,11 @@ def optimize_hparams(init_kwargs, train_config):
     )
 
     study.optimize(
-        Objective(init_kwargs, train_config), n_trials=100, timeout=60 * 60 * 24
+        lambda trial: objective(
+            trial, init_kwargs=init_kwargs, train_config=train_config
+        ),
+        n_trials=100,
+        timeout=60 * 60 * 24,
     )
 
     print("Number of finished trials: {}".format(len(study.trials)))
