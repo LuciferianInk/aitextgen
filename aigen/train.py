c53c28d1979c70f92fe07ab1b543bce983f8cc25
Luciferian Ink <LuciferianInk@protonmail.com>
2023-12-31 06:17:28 -0600

    update

--- aigen/train.py
+++ aigen/train.py
@@ -351,7 +351,7 @@ class AIGMetricsLogger(Callback):
         metrics = {
             "train_loss": trainer.callback_metrics["train_loss"],
             "lr": float(trainer.optimizers[0].param_groups[0]["lr"]),
-            "tokens": int(trainer.callback_metrics["train_tokens"]),
+            "train_tokens": int(trainer.callback_metrics["train_tokens"]),
         }
 
         if current_epoch > 0:
