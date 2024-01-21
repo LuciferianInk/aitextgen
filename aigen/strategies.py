eb3ae8eae8396c68f1be7eb4dbde1a5e68e6b207
Luciferian Ink <LuciferianInk@protonmail.com>
2024-01-21 14:57:32 -0600

    no hivemind compression for now

--- aigen/strategies.py
+++ aigen/strategies.py
@@ -67,7 +67,9 @@ def get_strategy(name, params, hparams, train_params, schedule):
 
         focus = os.environ["FOCUS"]
 
-        pet = random.choice(["cat", "dog", "fox", "jackal", "lemur"])
+        pet = random.choice(
+            ["cat", "dog", "fox", "jackal", "lemur", "squirrel", "frog"]
+        )
 
         schedule = False
         strategy = HivemindStrategy(
@@ -89,9 +91,9 @@ def get_strategy(name, params, hparams, train_params, schedule):
             delay_optimizer_step=True,
             offload_optimizer=True,
             reuse_grad_buffers=False,
-            grad_compression=Float16Compression(),
-            state_averaging_compression=Float16Compression(),
-            load_state_compression=NoCompression(),
+            # grad_compression=Float16Compression(),
+            # state_averaging_compression=Float16Compression(),
+            # load_state_compression=NoCompression(),
             scheduler_fn=partial(torch.optim.lr_scheduler.ExponentialLR, gamma=0.9999),
         )
 
